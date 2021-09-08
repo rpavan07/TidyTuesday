dd0783b6a5c5bd84ab849e075f852f2cef2ebd61
R Pavan <60320421+rpavan07@users.noreply.github.com>
2021-09-08 19:25:40 +0530

    Add files via upload
    update

---
+++ 2021_Week37/f1.R
@@ -0,0 +1,44 @@
+library(waffle)
+library(tidyverse)
+library(ggrepel)
+dfc <- read_csv("F:/Others/F1/champions.csv")
+
+dfc <- dfc%>%
+  select(Champions:Season) %>%
+  pivot_longer(-c(Champions)) 
+
+dfc$fill <- 1 
+
+theme_max <- function () {
+  theme_minimal(base_size = 9, base_family = "Consolas")%+replace%
+    theme(
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      plot.background = element_rect(fill = "cornsilk", colour = 'cornsilk')
+    )
+}
+
+ggplot(data = dfc, aes(fill=Champions, values=fill))+
+  geom_waffle(color = "cornsilk", size=.25, n_rows=9)+
+  facet_wrap(~Champions, nrow = 3, strip.position = "bottom", labeller = label_wrap_gen(6))+
+  theme_max()+
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand= c(0,0)) +
+  theme(legend.position =  "none",
+        axis.title.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_blank(),
+        panel.spacing = unit(2, 'lines'),
+        plot.title = element_text(face = "bold", size = 19, hjust = .5),
+        plot.subtitle = element_text(size = 11, hjust = .5),
+        plot.margin = margin(10,10,15,10))+
+  labs(fill = "",
+       x = "",
+       y = "",
+       title = "F1 Championships",
+       subtitle = "Each block represents one championship (from 1950 till 2020)",
+       caption = "by @thehopelesskiid // data via Ergast API")
+  
+
