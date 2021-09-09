c65272529e4d773786bd3961b0cbed4ffd987174
R Pavan <60320421+rpavan07@users.noreply.github.com>
2021-09-09 11:42:44 +0530

    update: used fct_infreq() to order it
    fct_infreq() orders factors according to their frequency in the data.

--- 2021_Week37/f1.R
+++ 2021_Week37/f1.R
@@ -4,6 +4,7 @@ library(tidyverse)
 dfc <- read_csv("F:/Others/F1/champions.csv")
 dfc$fill <- 1 
 dfc$Champions <- str_trim(dfc$Champions)
+dfc$Champions <- factor(dfc$Champions, levels=levels(fct_infreq(dfc$Champions)))
 
 theme_max <- function () {
   theme_minimal(base_size = 9, base_family = "Consolas")%+replace%
